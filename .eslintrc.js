ee82a2c6b54a8a332bf6b04ecf643797845f68eb
Martonosi Róbert <robert.martonosi@idomsoft.hu>
2022-08-28 11:52:24 +0200

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'linebreak-style': 0,
+    'no-param-reassign': 0,
+    'max-len': [2, 250, 4],
   },
   overrides: [
     {
