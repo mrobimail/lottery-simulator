096af3f4ed68f0eda83d8f47fb70aa2768a67674
Martonosi Róbert <robert.martonosi@idomsoft.hu>
2022-08-28 11:53:54 +0200

    Add Inside layout

---
+++ src/views/layout/Inside.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="green lighten-3" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-0">
+        <v-btn text class="text-capitalize title no-active" to="/">Lottery Simulator</v-btn>
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <router-view/>
+      </v-container>
+    </v-main>
+
+  </v-app>
+</template>
+
+<script>
+import Vuetify from 'vuetify';
+
+export default {
+  name: 'Inside',
+  vuetify: new Vuetify(),
+  data() {
+    return {
+      drawer: true,
+    };
+  },
+};
+</script>
--- src/vuetify.js
+++ src/vuetify.js
@@ -1,6 +1,23 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import colors from 'vuetify/lib/util/colors';
 import 'vuetify/dist/vuetify.min.css';
+import en from 'vuetify/es5/locale/en';
 
 Vue.use(Vuetify);
-export default new Vuetify({});
+
+const opts = {
+  lang: {
+    locales: { en },
+    current: 'en',
+  },
+  theme: {
+    themes: {
+      light: {
+        primary: colors.green.lighten3,
+      },
+    },
+  },
+};
+
+export default new Vuetify(opts);
