41055c463429a0c6210fe09f07a42d85c500aee1
Martonosi Róbert <robert.martonosi@idomsoft.hu>
2022-08-28 11:54:56 +0200

    Add LotterySimulator page and delete unused page

---
+++ src/views/lotterySimulator/LotterySimulator.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="lotterySimulator">
+    <v-card class="rounded-xl mb-4">
+      <v-card-text>
+        <h1>Result</h1>
+        <p>result component here</p>
+        <p>result match number component here</p>
+        <p>Winning numbers: numbers component here</p>
+        <p>Your numbers: numbers component here</p>
+        <p>Play with random numbers: checkbox</p>
+        <p>Speed sliders component here</p>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
