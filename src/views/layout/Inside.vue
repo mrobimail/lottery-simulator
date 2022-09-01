f5fc3b9bdb067e11a59023198ad91eb5fe5e78e4
Martonosi Róbert <robert.martonosi@idomsoft.hu>
2022-09-01 19:44:27 +0200

    Delete unused code

--- src/views/layout/Inside.vue
+++ src/views/layout/Inside.vue
@@ -1,7 +1,6 @@
 <template>
   <v-app id="inspire">
     <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="green lighten-3" dark>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title style="width: 300px" class="ml-0 pl-0">
         <v-btn text class="text-capitalize title no-active" to="/">Lottery Simulator</v-btn>
       </v-toolbar-title>
@@ -22,10 +21,5 @@ import Vuetify from 'vuetify';
 export default {
   name: 'Inside',
   vuetify: new Vuetify(),
-  data() {
-    return {
-      drawer: true,
-    };
-  },
 };
 </script>
